59cfbb40dffa01a9c84ec0d394561e75b65fb5a9
Josh Betzsold <bet16011@byui.edu>
2021-11-03 13:29:17 -0600

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Navigation.css';
+
+function Navigation() {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default Navigation;
---
+++ src/components/Summary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Summary.css';
+
+function Summary() {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default Summary;
